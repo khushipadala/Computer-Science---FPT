6c2b18124de534c2b301e39d855201f87f0f2078
khushipadala <47187058+khushipadala@users.noreply.github.com>
2019-06-19 01:05:26 -0400

    Update ball.js

--- ball.js
+++ ball.js
@@ -13,6 +13,7 @@ function Ball(){
     fill(255);
     ellipse(this.pos.x, this.pos.y, this.r*2);
   }
+
   this.update = function(){
     this.pos.add(this.acc);
 
@@ -20,6 +21,67 @@ function Ball(){
       this.acc.y*=-1;
     }
   }
+
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    this.acc.y = constrain(this.acc.y, -thismaxSpd.y, this.maxSpd.y);
+    this.acc.y = constrain(this.acc.y, -thismaxSpd.y, this.maxSpd.y);
+  }else if(this.collObj){
+    let d = dist(this.pos.x, this.pos.y, this.collObj.pos.x, this.collObj.pos.Y);
+    if(d>100){this.collision = face;}
+  }
+}
+
+this.scores = function(){
+  if(this.pos.x<-this.r){
+    AIScore++;
+    this.res();
+  }else if(this.pos.x>width+this.r){
+    player++;
+    this.res();
+  }
+}
+
+this.res = function(){
+  go = false;
+
+  ai.pos = createVector(width-si.w*3, height/2-ai.h/2);
+  player.pos = createVector(player.w*2, height/2-player.h/2);
+
+  this.pos= createVector(width/2, height/2);
+
+  do{
+    this.acc = p5.Vector.random2D();
+    this.acc.setMag(random(4,6));
+  }while(abs(this.acc.x)<3 || abs(this.acc.y)<3);
+  }
+
+}
     
     
