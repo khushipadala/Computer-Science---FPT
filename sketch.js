b7371603fc9996b775fee70672445420af8ab499
khushipadala <47187058+khushipadala@users.noreply.github.com>
2019-06-19 00:10:24 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
+var player, ball;
+
+var playerScore = 0;
+var AIScore = 0;
+
 var dots = [];
 var dSize = 15;
+var txtSize = 30;
 
 function setup(){
 	creatCanvas(800,600);
+
+	player = new Player;
+	ball = new Ball
 
 	for(let y = dSize/2; y=height; y+=dSize*2){
 		dots.push(createVector(width/2-dSize/2,));
@@ -15,11 +24,29 @@ function draw(){
 	background(0);
 
 	noStroke();
-	fill(255);
+	fill(255, 100);
 	drawSquares();
 
 	player.update();
 	player.show();
+
+	ball.update();
+	ball.show();
+
+	drawScores();
+}
+
+function drawScores(){
+	let x1 = width/4;
+	let x2 = width*3/4;
+	let y = txtSize*1.5;
+
+	noStroke();
+	sill(255);
+	textAlign(CENTER);
+	textSize(txtSize);
+	text(playerScore, x1, y);
+	text(AIscore, x2, y)
 }
 
 function drawSquares(){
